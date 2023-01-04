26691d176b7ef2c4b8dfb66706dc8564a5eda888
levib00 <levib8@hotmail.com>
2023-01-04 17:55:51 -0500

    Add function to encrypt ideal string

--- scripts/caesar-cipher.js
+++ scripts/caesar-cipher.js
@@ -1,7 +1,4 @@
-
-
-export const caesarCipher = (shift) => {
-  const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const encode = (shift, alphabet) => {
   if (shift >= 0) {
     for (let i = 0; i < shift; i++) {
       alphabet.unshift(alphabet.pop())
@@ -12,4 +9,18 @@ export const caesarCipher = (shift) => {
     }
   }
   return alphabet
+}
+
+export const caesarCipher = (shift, str) => {
+  const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const indexArray = []
+  for (let i = 0; i < str.length; i ++) {
+    indexArray.push(alphabet.indexOf(str.charAt(i)))
+  }
+  const encodedAlphabet = encode(shift, alphabet);
+  let encodedString = ''
+  for (let index of indexArray) {
+    encodedString = encodedString += encodedAlphabet[index]
+  }
+  return encodedString
 }
